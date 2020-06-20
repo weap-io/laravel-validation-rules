f4f683cf04687670ce4c8d5f6f465f87e2f6fc01
Alexandru Bugarin <alexandru.bugarin@gmail.com>
2020-06-20 18:00:02 +0200

    Implement and test rule to validate AWS S3 Bucket name

---
+++ src/Rules/Services/Aws/S3BucketName.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Weap\LaravelValidationRules\Rules\Services\Aws;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class S3BucketName implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return (bool) (preg_match('/^(?=^.{3,63}$)(?!^(\d+\.)+\d+$)(^(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])$)/', $value) == 1);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be a valid S3 bucket name.';
+    }
+}
---
+++ tests/Services/Aws/S3BucketNameTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Weap\LaravelValidationRules\Tests\Rules\Services\Aws;
+
+use Weap\LaravelValidationRules\Tests\TestCase;
+use Weap\LaravelValidationRules\Rules\Services\Aws\S3BucketName;
+
+class S3BucketNameTest extends TestCase
+{
+    /** @test */
+    public function it_passes_if_bucket_name_is_valid()
+    {
+        $rule = (new S3BucketName);
+
+        $this->assertTrue($rule->passes('bucket_name', 'my-backups'));
+        $this->assertTrue($rule->passes('bucket_name', 'abc'));
+        $this->assertTrue($rule->passes('bucket_name', '123'));
+        $this->assertTrue($rule->passes('bucket_name', 'abc.123'));
+    }
+
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_has_less_than_3_characters_length()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', 'my'));
+    }
+    
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_has_more_than_63_characters_length()
+    {
+        $name = str_repeat('abcdefgh', 8); // 64 characters
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', $name));
+    }
+
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_has_one_or_more_not_allowed_characters()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', 'abc$%'));
+    }
+    
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_starts_with_an_uppercase_letter()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', 'Abc'));
+    }
+    
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_ends_with_an_uppercase_letter()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', 'abC'));
+    }
+
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_labels_are_not_sepparated_by_a_period()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', 'abc def'));
+    }
+    
+    /** @test */
+    public function it_does_not_pass_if_bucket_name_is_formatted_as_an_ip_address()
+    {
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', '192.168.5.4'));
+        $this->assertFalse((new S3BucketName)->passes('bucket_name', '127.0.0.1'));
+    }
+}
